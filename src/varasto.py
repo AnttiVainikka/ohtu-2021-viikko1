4c1ef2aaaafad42e23d0b2d1ba02b481723032c7
Antti Vainikka <antti.vainikka36@gmail.com>
2021-11-06 02:00:39 +0200

    Virheen committaus

--- src/varasto.py
+++ src/varasto.py
@@ -42,4 +42,4 @@ class Varasto:
         return maara
     
     def __str__(self):
-        return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
+        return f"suuri vääryys saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
